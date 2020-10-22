72db16cb7e019f4cd11d8d0f5ee962a64f38ed96
AnYelg <73191386+AnYelg@users.noreply.github.com>
2020-10-22 15:12:26 -0500

    Update resumen_cod.py

--- Entrega_final/resumen_cod.py
+++ Entrega_final/resumen_cod.py
@@ -1,12 +1,12 @@
 import streamlit as st
 def prin (): 
     st.title("Resumen del codigo hecho")
-    st.markdown("Muchos tipos de rocas son expulsadas por un volcán al momento de la explosión (como obsidiana, lapilli, escoria, etc.). Estas rocas pueden medir de 2mm hasta 7mm aproximadamente. (Sieron, s.f.) Este proyecto se enfocará en el Popocatépetl. Los proyectiles lanzados por este volcán tienen angulos entre 40° y 90° (del Pozzo, 2017). Tiene una altura de 5426 m sobre el nivel del mar y una prominencia de 3800 m. (del Pozzo, 2017)")
+    st.markdown("Muchos tipos de rocas son expulsadas por un volcán al momento de la explosión (como obsidiana, lapilli, escoria, etc.). Estas rocas pueden medir de 2mm hasta 7mm aproximadamente. (Sieron, s.f.) Este proyecto se enfocará en el Popocatépetl. Los proyectiles lanzados por este volcán tienen ángulos entre 40° y 90° (del Pozzo, 2017). Tiene una altura de 5426 m sobre el nivel del mar y una prominencia de 3800 m. (del Pozzo, 2017)")
     st.markdown("El rango de velocidad fue recuperado de un documento de la UNAM.")
-    st.image("Tabla2.jpg",caption="Tabla de datos recuperados de videos de erupciones del Popocatéptl. Se utilizaron como parametro la velocidad mínima y máxima encontradas en esta tabla ubicada en la página 82. (del Pozzo, 2017)")
-    st.markdown("En la primera parte del código se tienen que importar las librerias que se ocuparan y el nombre en el cual lo vas a exportar, por ejemplo numpy se exportó con el nombre de np. Numpy ayuda para escribir raices cuadradas, seno y coseno. Matplot podrá plasmar las gráficas necesarias.")
+    st.image("Tabla2.jpg",caption="Tabla de datos recuperados de videos de erupciones del Popocatépetl. Se utilizaron como parámetro la velocidad mínima y máxima encontradas en esta tabla ubicada en la página 82. (del Pozzo, 2017)")
+    st.markdown("En la primera parte del código se tienen que importar las librerías que se ocuparan y el nombre en el cual lo vas a exportar, por ejemplo, numpy se exportó con el nombre de np. Numpy ayuda para escribir raíces cuadradas, seno y coseno. Matplot podrá plasmar las gráficas necesarias.")
     st.image("imagen1.jpg")
-    st.markdown("Se va a crear una función, la cuál falicitará hacer la gráfica más adelante. Lo que hace la función es convertir los grados a radianes, calcular la velocidad inicial en x, calcular la velocidad inicial en y, calcular la posición inicial en x y calcular la posición inicial en y.")
+    st.markdown("Se va a crear una función, la cual facilitará hacer la gráfica más adelante. Lo que hace la función es convertir los grados a radianes, calcular la velocidad inicial en x, calcular la velocidad inicial en y, calcular la posición inicial en x y calcular la posición inicial en y.")
     st.latex(r"""v_{ox} = v_o\cos\theta""")
     st.latex(r"""v_{oy} = v_o\sin\theta""")
     st.latex(r"""r_{ox} = v_{ox}t""")
@@ -14,16 +14,16 @@ def prin ():
     st.image("imagen2.jpg")
     st.markdown("Se establecerá la variable de las posiciones igual a la función para mandarla llamar, y poder crear una gráfica correctamente.")
     st.image("imagen2_1.jpg")
-    st.markdown("Toda la siguiente parte es el código para crear la gráfica, poner el titulo, una cuadricula y los limites de x y y.")
+    st.markdown("Toda la siguiente parte es el código para crear la gráfica, poner el título, una cuadricula y los límites de “x” y “y”.")
     st.image("imagen3.jpg")
     st.image("graf1.jpg",caption="Gráfica sin resistencia al aire")
-    st.markdown("En la siguiente parte del código se establecen las variables que afectarán al proyectil en un caso de la vida real. Para saber las dimensiones de un balístico lanzado por el Popocatépetl, se hizo un análisis de fragmentos de proyectiles con mayor distancia del cráter. Fueron mandados a un laboratorio para calcular mejor los datos. (del Pozzo, 2017) ")
-    st.image("Tabla1.jpg",caption="Tabla de características de balísticos del Popocatéptl localizada en la página 76.(del Pozzo, 2017)")
+    st.markdown("En la siguiente parte del código se establecen las variables que afectarán al proyectil en un caso de la vida real. Para saber las dimensiones de un balístico lanzado por el Popocatépetl, se hizo un análisis de fragmentos de proyectiles con mayor distancia del cráter. Fueron mandados a un laboratorio para calcular mejor los datos. (del Pozzo, 2017).")
+    st.image("Tabla1.jpg",caption="Tabla de características de balísticos del Popocatépetl localizada en la página 76.(del Pozzo, 2017)")
     st.image("imagen4.jpg")
-    st.markdown("Vamos a definir D, que es la contante de la resitencia al aire. La equación es la siguiente:")
+    st.markdown("Vamos a definir D, que es la contante de la resistencia al aire. La ecuación es la siguiente:")
     st.latex(r"""D = \frac{\rho \cdot C\cdot A}{2} """ )
     st.markdown(r"""
-    Volveremos a crear una función que nos ayudará a calcular los valores necesarios para hacer la gráfica con resistencia al aire. 
+    Volveremos a crear una función que nos ayudará a calcular los valores necesarios para hacer la gráfica con resistencia al aire.
     
     1. Convertir los grados a radianes.   
     2. Calcular la velocidad inicial en x. $v_{ox} = v_o\cos\theta$   
